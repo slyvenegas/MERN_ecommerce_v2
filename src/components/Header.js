c4f2ed5342b6140bc9443d512e9049180b04d1e3
slyvenegas <venegas@gmail.com>
2024-06-15 16:26:31 -0500

    Primeros elementos Login

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Logo from './Logo'
 import { HiSearchCircle } from "react-icons/hi";
 import { HiUserCircle } from "react-icons/hi";
 import { IoIosCart } from "react-icons/io";
-
+import { Link } from 'react-router-dom';
 
 
 
@@ -11,13 +11,15 @@ const Header = () => {
   return (
     <header className='h-16 shadow-md bg-slate-200'>
       <div className='h-full container mx-auto flex items-center px-4 justify-between'>
-        <div className=''>
-          <Logo w={90} h={50} />
+        <div className='cursor-pointer'>
+          <Link to={"/"}>
+            <Logo w={100} h={60} />
+          </Link>
         </div>
 
         <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
           <input type='text' placeholder='Search Product' className='w-full outline-none rounded-l-full' />
-          <div className='text-lg min-w-[50px] h-8 bg-red-300 flex items-center justify-center rounded-r-full text-white hover:bg-red-600'>
+          <div className='text-lg min-w-[50px] h-6 bg-red-300 flex items-center justify-center rounded-r-full text-white hover:bg-red-600'>
             <HiSearchCircle />
           </div>
         </div>
@@ -27,15 +29,15 @@ const Header = () => {
             <HiUserCircle />
           </div>
 
-          <div className='text-2xl relative'>
+          <div className='text-2xl relative cursor-pointer'>
             <span><IoIosCart /></span>
-            <div className='bg-red-300 h-5 w-5 rounded-full p-1 flex items-center justify-center absolute -top-3 -right-2 hover:bg-red-600w'>
+            <div className='bg-red-300 h-5 w-5 rounded-full p-1 flex items-center justify-center absolute -top-3 -right-2 hover:bg-red-600'>
               <p className='text-sm'>0</p>
             </div>
           </div>
 
           <div>
-            <button className='bg-red-300 text-white py-1 px-4 rounded-full hover:bg-red-600'>Login</button>
+            <Link to={"/login"} className='bg-red-300 text-white py-1 px-4 rounded-full hover:bg-red-600'>Login</Link>
           </div>
 
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import loginIcons from '../assest/signin.gif'
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <section id='login'>
+      <div className='mx-auto container p-4'>
+
+        <div className='bg-slate-100 p-2 w-full max-w-md mx-auto'>
+          <div className='w-20 h-20 mx-auto'>
+            <img src={loginIcons} alt='Login icons' />
+          </div>
+
+          <form className='grid'>
+            <div>
+              <label>Email :</label>
+              <div className='bg-slate-200 p-2'>
+                <input type='email' placeholder='Enter your email' className='w-full h-full outline-none bg-transparent'></input>
+              </div>
+            </div>
+
+            <div>
+              <label>Password :</label>
+              <div className='bg-slate-200 p-2'>
+                <input type='password' placeholder='Enter your password' className='w-full h-full outline-none bg-transparent'></input>
+              </div>
+            </div>
+
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </section>
   )
 }
 
