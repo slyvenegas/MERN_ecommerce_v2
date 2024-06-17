8670b2e64c1fa80d7ddc5fcafd1c5c006db30110
slyvenegas <venegas@gmail.com>
2024-06-16 20:14:06 -0500

    Sign up finalizado / funcionalidad cargar img perfil

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,18 +3,21 @@ import loginIcons from '../assest/signin.gif'
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
 import { Link } from 'react-router-dom'
+import imageTobase64 from '../helpers/imageTobase64';
 
 const SignUp = () => {
 
   const [showPassword, setShowPassword] = useState(false)
   const [showConfirmPassword, setShowConfirmPassword] = useState(false)
 
+
+
   const [data, setData] = useState({
-    email : "",
-    password : "",
-    name : "",
-    confirmPassword : "",
-    profilePic : "",
+    email: "",
+    password: "",
+    name: "",
+    confirmPassword: "",
+    profilePic: "",
 
   })
 
@@ -23,25 +26,54 @@ const SignUp = () => {
 
     setData((preve) => {
       return {
-          ...preve,
-          [name] : value
+        ...preve,
+        [name]: value
       }
     })
   }
+
+
+  const handleUploadPic = async (e) => {
+    const file = e.target.files[0]
+
+    const imagePic = await imageTobase64(file)
+    // console.log("imagePic", imagePic)
+
+    setData((preve)=>{
+      return {
+        ...preve,
+        profilePic : imagePic
+      }
+    })
+  }
+
+
+
 
   const handleSubmit = (e) => {
     e.preventDefault()
   }
 
-  console.log("data login",data)
+  console.log("data login", data)
 
   return (
     <section id='signup'>
       <div className='mx-auto container p-4'>
 
         <div className='bg-slate-100 p-5 w-full max-w-sm mx-auto'>
-          <div className='w-20 h-20 mx-auto'>
-            <img src={loginIcons} alt='Login icons' />
+          <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
+            <div>
+              <img src={data.profilePic || loginIcons} alt='Login icons' />
+            </div>
+
+            <form>
+              <label>
+                <div className='text-xs bg-opacity-80 pb-4 mt-2 cursor-pointer bg-slate-200 py-3 text-center absolute bottom-0 w-full'>
+                  Upload Photo
+                </div>
+                <input type='file' className='hidden' onChange={handleUploadPic} />
+              </label>
+            </form>
           </div>
 
           <form className='pt-6 flex flex-col gap-3' onSubmit={handleSubmit}>
@@ -54,6 +86,7 @@ const SignUp = () => {
                   name='name'
                   value={data.name}
                   onChange={handleOnChange}
+                  required
                   className='w-full h-full outline-none bg-transparent'></input>
               </div>
             </div>
@@ -67,6 +100,7 @@ const SignUp = () => {
                   name='email'
                   value={data.email}
                   onChange={handleOnChange}
+                  required
                   className='w-full h-full outline-none bg-transparent'></input>
               </div>
             </div>
@@ -80,6 +114,7 @@ const SignUp = () => {
                   name='password'
                   value={data.password}
                   onChange={handleOnChange}
+                  required
                   className='w-full h-full outline-none bg-transparent'></input>
                 <div className='cursor-pointer text-xl' onClick={() => setShowPassword((preve) => !preve)}>
                   <span>
@@ -108,6 +143,7 @@ const SignUp = () => {
                   name='confirmPassword'
                   value={data.confirmPassword}
                   onChange={handleOnChange}
+                  required
                   className='w-full h-full outline-none bg-transparent'></input>
                 <div className='cursor-pointer text-xl' onClick={() => setShowConfirmPassword((preve) => !preve)}>
                   <span>
